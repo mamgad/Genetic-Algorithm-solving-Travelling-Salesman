43b46ac6553e8a9ad4651702251d3199d4f8c527
Unknown <moh_amgad@hotmail.com>
2017-03-25 01:24:19 +0200

    Building Classes

--- Main.py
+++ Main.py
@@ -9,16 +9,6 @@ list_of_nodes=[]
 list_of_edges=[]
 list_of_paths=[]
 
-def first_generation():
-    edges = range(0, 30), range(0, 30), 0
-    random.shuffle(edges[0])
-    random.shuffle(edges[1])
-    print edges[0]
-    print edges[1]
-
-
-first_generation()
-
 
 
 def generate_random_graph():
@@ -49,8 +39,9 @@ for i in range(0, 29):
 
     list_of_edges.append(edge1)
     print str(i)+'\t\t'+str(edge1.cost)
-print total_cost
-list_of_paths.append(list_of_edges,total_cost)
+print 'total cost\t'total_cost
+path1=Graph.path(list_of_edges,total_cost)
+list_of_paths.append(path1)
 
 
 
