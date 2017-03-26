9f048e68fed4cb51d826b88027ece31c3eea14b6
Unknown <moh_amgad@hotmail.com>
2017-03-26 16:21:12 +0200

    Add Mutation

--- Graph.py
+++ Graph.py
@@ -1,17 +1,52 @@
 import math
+import random
 class node:
-    def __init__(self, x_loc, y_loc):
+    def __init__(self, x_loc, y_loc,number):
         self.x=x_loc
         self.y=y_loc
+        self.number=number
 class edge:
-    def __init__(self, first_node,second_node ,edge_cost):
+    def __init__(self, first_node,second_node):
         self.a = first_node
         self.b = second_node
-        self.cost=edge_cost
+        self.cost=dist(first_node,second_node)
 class path:
     def __init__(self, all_edges,total_cost):
         self.edges=all_edges
         self.cost=total_cost
+        self.nodes=[]
+        for i in range(0,len(self.edges)):
+            self.nodes.append(self.edges[i].a)
+        last_node =self.edges[len(self.edges)-1].b
+        self.nodes.append(last_node)
+    def dump(self):
+
+
+        for i in range(len(self.nodes)):
+            print str(i)+'\t'+str(self.nodes[i].number)
+    def mutate(self):
+        rand_i=random.randint(0,10)
+        print "rand_i is "+str(rand_i)
+        for i in range (0,rand_i):
+            rand1=random.randint(0,29)
+            rand2=random.randint(0,29)
+            print "Swapping "+str(rand1)+" with "+str(rand2)
+            temp_node=self.nodes[rand1]; '''Swapping nodes (mutating)'''
+            self.nodes[rand1]=self.nodes[rand2]
+            self.nodes[rand2]=temp_node
+        return self
+    def getCost(self):
+        total_cost=0
+        for i in range(0, len(self.nodes)-1):
+            first_node = self.nodes[i];
+            second_node = self.nodes[i + 1];
+
+            cost = dist(first_node, second_node)
+            total_cost += cost
+        return total_cost
+
+
+
 
 
 
--- Main.py
+++ Main.py
@@ -8,9 +8,11 @@ import matplotlib
 list_of_nodes = []
 list_of_edges = []
 list_of_paths = []
+new_gen_paths = []
 best_path=None
 r_map=None
 crossover_mapping=None
+
 def crossover():
     for i in range(0,20):
         list_of_paths[crossover_mapping[0]].edges
@@ -18,19 +20,25 @@ def random_map():#CROSS OVER RATE !!
     global r_map
     global crossover_mapping
     r_map=random.sample(range(30), 8),random.sample(range(30), 8)
-    print r_map
+    #print r_map
     crossover_mapping=random.sample(range(100), 20),random.sample(range(100), 20)
-    print crossover_mapping
+    #print crossover_mapping
 def generate_random_graph():
     global list_of_nodes
+    print "Node#\tX-axis\tY-axis"
     for i in range(0, 30):  # 30 Random Points
         randX = random.randint(1, 102400)
         randY = random.randint(1, 102400)
-        node1 = Graph.node(randX, randY)
+        node_number=i
+        node1 = Graph.node(randX, randY,node_number)
         list_of_nodes.append(node1)
-        print 'Node ' + str(i) + ' Has been added at ' + str(randX) + ',' + str(randY)
-generate_random_graph()
-print '============\n\nDistance between A and B = ', Graph.dist(list_of_nodes[0], list_of_nodes[1])
+
+        print str(node_number) + '\t\t' + str(randX) + '\t' + str(randY)
+def show_paths_costs():
+    print "\n===================\nPATH#\tPATH COST"
+    for i in range(0, 100):
+        print str(i)+'\t\t'+str(sorted_pathes[i].cost)
+    print "\n==================="
 def traverse():
     global list_of_nodes
     global list_of_edges
@@ -41,7 +49,7 @@ def traverse():
     # plt.show()
 
 
-    print 'Edge#\tCost'
+    #print 'Edge#\tCost'
     total_cost = 0;
     i=0
     for i in range(0, 29):
@@ -50,34 +58,46 @@ def traverse():
 
         cost = Graph.dist(first_node, second_node)
         total_cost += cost
-        edge1 = Graph.edge(first_node, second_node, cost)
+        edge1 = Graph.edge(first_node, second_node)
 
         list_of_edges.append(edge1)
-        print str(i) + '\t\t' + str(edge1.cost)
-    print 'total cost\t' + str(total_cost)
+        #print str(i) + '\t\t' + str(edge1.cost)
+    #print 'total cost\t' + str(total_cost)
 
     path1 = Graph.path(list_of_edges, total_cost)
     list_of_paths.append(path1)
-
+    print "Test\t"+str(path1.cost)+'\t'+str(path1.getCost())
 
     list_of_edges = []
 
-for j in range(0,100):
-    traverse()
-    sorted_pathes = sorted(list_of_paths, key=lambda x: x.cost, reverse=False)
-    min_of_generation = sorted_pathes[0]
-    print sorted_pathes[0].cost
-    if (best_path is None or best_path.cost > min_of_generation):
-        best_path = min_of_generation #UPDATE BEST PATH
-    '''NEW GENERATION STARTS'''
-    random_map()
-print "Count is " + str(len(list_of_paths))
 
 
+generate_random_graph()
+def mutate_generation():
+    for i in range(0,len(list_of_paths)):
+        if(random.random<0.2):#20% Mutation Rate
+            list_of_paths[i].mutate()
 
+def generate():
+    for j in range(0, 100):  # Find 100 Random path
+        global list_of_paths
+        global min_of_generation
+        global best_path
 
+        traverse()
+        sorted_pathes = sorted(list_of_paths, key=lambda x: x.cost, reverse=False)
+        min_of_generation = sorted_pathes[0]
+        print sorted_pathes[0].cost
+        if (best_path is None or best_path.cost > min_of_generation):
+            best_path = min_of_generation  # UPDATE BEST PATH
 
+        '''NEW GENERATION STARTS'''
+        random_map()
+    print "Count is " + str(len(list_of_paths))
 
-
-
-
+generate()
+list_of_paths[0].dump()
+print "Mutation Test"
+list_of_paths[0].mutate()
+list_of_paths[0].dump()
+show_paths_costs()
